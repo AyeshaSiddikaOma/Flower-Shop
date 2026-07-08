2f9db7bd8c99c105d01230217328878b18b1f268
ayeshasiddikaoma-cmyk <ayeshasiddikaoma@gmail.com>
2026-07-08 09:12:27 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+$('.sliders').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  arrow:false,
+  prevArrow:'<i class="fa-solid fa-angles-left left"></i>',
+nextArrow:'<i class="fa-solid fa-angles-right right" ></i>',
+
+});
+
+// MixItUp
+var mixer = mixitup('#homePart .row.mt-5');
